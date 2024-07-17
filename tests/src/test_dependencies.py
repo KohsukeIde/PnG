4227ed5bde0472386fba83403274deaef0d48e25
KohsukeIde <kohsukeide0726@gmail.com>
2024-07-17 10:43:32 +0900

    apply: apply code format

--- tests/src/test_dependencies.py
+++ tests/src/test_dependencies.py
@@ -2,6 +2,8 @@ import cv2
 
 
 def test_dependencies():
+    """Test dependencies.
+    for check dependencie's library version.
+    """
     version = cv2.__version__
     assert version == "4.10.0"
-    
