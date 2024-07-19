bc86467e0b67e23a2176e2299857a2f267f9d7b9
ueda0319 <i.ueda0319@gmail.com>
2024-07-19 17:00:18 +0900

    fix 2dgaussian fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,15 +15,20 @@ def twod_gaussians() -> TwoDGaussians:
     variance_max: Final[float] = 4.0
     color_max: Final[float] = 255.0
     alpha_max: Final[float] = 10.0
+    # Generate gaussian parameters tensor
+    mean: np.ndarray = np.random.rand(k, 2) * mean_max
+    color: np.ndarray = np.random.rand(k, 3) * color_max
+    alpha: np.ndarray = np.random.rand(k) * alpha_max
+    # Generate covariance with restriction
+    cov_s: np.ndarray = np.zeros((k, 2, 2))
+    cov_s[:, 0, 0] = np.random.rand(k) * variance_max + 1.0
+    cov_s[:, 1, 1] = np.random.rand(k) * variance_max + 1.0
+    theta: np.ndarray = np.random.rand(k) * np.pi
+    cov_r: np.ndarray = np.array(
+        [[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]
+    ).transpose(2, 0, 1)
+    cov_rs = np.matmul(cov_r, cov_s)
+    cov = np.matmul(cov_rs, cov_rs.transpose(0, 2, 1))
     # Generate 2D Gaussians
-    obj = TwoDGaussians(
-        np.random.rand(k, 2) * mean_max,
-        np.random.rand(k, 2, 2) * variance_max,
-        np.random.rand(k, 3) * color_max,
-        np.random.rand(k) * alpha_max,
-    )
-    # Apply covariances restrictions
-    obj.covs[:, 0, 0] = np.abs(obj.covs[:, 0, 0])
-    obj.covs[:, 1, 1] = np.abs(obj.covs[:, 1, 1])
-    obj.covs[:, 0, 1] = -obj.covs[:, 1, 0]
+    obj = TwoDGaussians(mean, cov, color, alpha)
     return obj
