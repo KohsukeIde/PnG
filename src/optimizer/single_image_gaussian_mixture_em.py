fix : removed normalization for new color

src/optimizer/single_image_gaussian_mixture_em.py
@@ -47,18 +47,29 @@ class SingleImageGaussianMixtureEM:
 
         # Initialize positions randomly
         means = np.random.rand(n_gaussians, 2) * [height, width]
-
+        
+        # Initialize rotation angles randomly
+        # rotation_angles = np.random.uniform(0, 2*np.pi, n_gaussians)
+        
+        # Initialize scales with constant for now
+        # scale_const = np.sqrt(min(height, width)/10)
+        # scale_x = np.full(n_gaussians, scale_const) #[n_gaussian, 1]
+        # scale_y = np.full(n_gaussians, scale_const) #[n_gaussian, 1]
+        
         # Initialize covariances with constant for now
         cov_const = min(height, width)/10 # adjustable  constant
         covs = np.array([np.eye(2) * cov_const for _ in range(n_gaussians)])
+        # covs = np.array([TwoDGaussians.params_to_cov(angle, sx, sy) for angle, sx, sy in zip(rotation_angles, scale_x, scale_y)])
+        
         # Initialize RGB values from the image
         rgb = np.array([self.image[int(y), int(x)] for y, x in means])
-
+        print(f"{rgb=}")
+        print(f"np.max{rgb=}")
         # Initialize alpha values
         alpha = np.full(n_gaussians, alpha_0)
         alpha /= np.sum(alpha)
 
-        return TwoDGaussians(means, covs, rgb, alpha)
+        return TwoDGaussians(means, covs ,rgb, alpha)
 
     def gaussian_pdf(
         self, mean: np.ndarray, cov: np.ndarray, height: int, width: int
@@ -111,6 +122,7 @@ class SingleImageGaussianMixtureEM:
                     )
                 )
         return n
+    
 
     def e_step(self, gaussians: TwoDGaussians) -> np.ndarray:
         """Compute the responsibilities (gamma) for each pixel and each Gaussian."""
@@ -232,6 +244,11 @@ class SingleImageGaussianMixtureEM:
         #                 diff, diff
         #             )
         # new_covs = new_covs * n_k_reciprocal[:, None, None]
+        
+        # Apply constraints to covariance matrices
+        # params = [TwoDGaussians.cov_to_params(cov) for cov in new_covs]
+        # new_rotation_angles, new_scale_x, new_scale_y = zip(*params)
+
 
         # Ensure covariance matrices are positive definite
         epsilon = 1e-6
@@ -267,7 +284,7 @@ class SingleImageGaussianMixtureEM:
         new_colors = np.sum(
             self.image[:, :, None, :] * gamma[:, :, :, None], axis=(0, 1)
         )
-        new_colors = new_colors * n_k_reciprocal[:, None]
+        # new_colors = new_colors * n_k_reciprocal[:, None]
 
         # new_colors = np.zeros((k, 3))
         # for i in range(k):

src/rasterizer/vanilla_2d_rasterizer.py
@@ -48,19 +48,10 @@ class Vanilla2DRasterizer:
         )
         # Initialize image with 0
         img = np.zeros((self.height * self.width, 3), np.float64)
-
-        # print(f"Number of Gaussians: {gaussians.k}")
-        # print(f"Means shape: {gaussians.means.shape}")
-        # print(f"Covs shape: {gaussians.covs.shape}")
-        # print(f"RGB shape: {gaussians.rgb.shape}")
-        # print(f"Alpha shape: {gaussians.alpha.shape}")
-
         for k in range(gaussians.k):
             cov_det = np.linalg.det(gaussians.covs[k, :, :])
             if cov_det < 0:
-                print(f"Warning: Negative covariance determinant for Gaussian {k}")
-                continue
-
+                raise ValueError("Covariance determinant should positive.")
             cov_inv = np.linalg.inv(gaussians.covs[k, :, :])
             # Scaled Color (ndarray[1, 3])
             scaled_color = (
@@ -69,17 +60,6 @@ class Vanilla2DRasterizer:
                 * gaussians.alpha[k]
                 * gaussians.rgb[k, None, :]
             )
-            # scaled_color = (
-            #     gaussians.alpha[k]
-            #     * gaussians.rgb[k, None, :]
-            # )
-            # print(f"Gaussian {k}:")
-            # print(f"  Mean: {gaussians.means[k]}")
-            # print(f"  Cov: {gaussians.covs[k]}")
-            # print(f"  RGB: {gaussians.rgb[k]}")
-            # print(f"  Alpha: {gaussians.alpha[k]}")
-            # print(f"  Scaled color: {scaled_color}")
-
             # Coordinates with gaussian's mean as origin (ndarray[height * width, 2])
             xy_k = xy - gaussians.means[k, None, :]
             # Normalized coordinates (ndarray[height*width, 2])
@@ -87,7 +67,7 @@ class Vanilla2DRasterizer:
             img += scaled_color * np.exp(-xy_n)
 
         # Reshape and convert to save image
-        img = img * 255 # 0-255 scaling
+        img = img / img * 255
         img_cv = img.reshape(self.height, self.width, 3).clip(0, 255).astype(np.uint8)
         if save_to_file:
             cv2.imwrite("outputs/tmp.png", img_cv)